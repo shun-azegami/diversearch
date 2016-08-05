f23f50b8b830bb0f4bbde117730518c068637055
tech-camp <tech-camp@tech-camp-046-no-MacBook-Air.local>
2016-08-05 18:33:07 +0900

    rename actions (jobs#show => jobs#index), delete /jobs/show.html.erb

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -3,10 +3,7 @@ class JobsController < ApplicationController
   def search
   end
 
-  # def index ...ajaxを使うのであれば不要
-  # end
-
-  def show
+  def index
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :jobs, only: [:search, :show]
+  resources :jobs, only: [:search, :index]
   resources :users, only: [:show]
   root 'jobs#search'
 end
